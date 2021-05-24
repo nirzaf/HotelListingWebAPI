6c23e059e311c227763c705b4eccddf5fb4fa99e
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-24 19:37:17 +0530

    LoginUserDTO class Added

--- HotelListing/Models/UserDTO.cs
+++ HotelListing/Models/UserDTO.cs
@@ -6,16 +6,6 @@ using System.Threading.Tasks;
 
 namespace HotelListing.Models
 {
-    public class LoginUserDTO
-    {
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-
-        [Required]
-        [StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
-        public string Password { get; set; }
-    }
 
     public class UserDTO : LoginUserDTO
     {
