3e58bc01af9fc482ec78d02cefc86a77bdbd8ecc
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-28 15:35:03 +0530

    private const int MaxPageSize = 50;

--- HotelListing/Models/RequestParams.cs
+++ HotelListing/Models/RequestParams.cs
@@ -2,14 +2,14 @@
 {
     public class RequestParams
     {
-        const int maxPageSize = 50;
+        private const int MaxPageSize = 50;
         public int PageNumber { get; set; } = 1;
         private int _pageSize = 10;
 
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
     }
 }
